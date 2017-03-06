93589a0a4a14761f560006723c25c3c567f947ad
sigvaldm <sigvaldm@fys.uio.no>
2017-03-06 11:55:52 +0100

    First seemingly correct langmuir oscillation after refactoring

--- demoLangmuir.py
+++ demoLangmuir.py
@@ -1,32 +1,70 @@
-from dolfin import *
-import numpy as np
-import matplotlib.pyplot as plt
-import time
-
+# Imports important python 3 behaviour to ensure correct operation and
+# performance in python 2
+from __future__ import print_function, division
 import sys
-#sys.path.append('../src')
-#from punc import *
-
-from punc import *
-
 if sys.version_info.major == 2:
+	from itertools import izip as zip
 	range = xrange
 
-#==============================================================================
-# GENERATE MESH
-#------------------------------------------------------------------------------
-
-print "Initializing solver"
-
-Ld = 6.28*np.array([1,1])			# Length of domain
-Nc = 32*np.array([1,1])				# Number of 'rectangles' in mesh
-mesh = RectangleMesh(Point(0,0),Point(Ld),*Nc)
-#mesh = Mesh("mesh/nonuniform.xml")
+from dolfin import *
+from punc import *
+from numpy import pi
+from matplotlib import pyplot as plt
 
 #==============================================================================
-# SOLVER
+# INITIALIZING FENICS
 #------------------------------------------------------------------------------
 
-solver = PoissonSolver(mesh,Ld,PeriodicBoundary(Ld))
+nDims = 2							# Number of dimensions
+Ld = 6.28*np.ones(nDims)			# Length of domain
+Nr = 32*np.ones(nDims,dtype=int)	# Number of 'rectangles' in mesh
 
+#mesh = RectangleMesh(Point(0,0),Point(Ld),*Nr)
+mesh = Mesh("mesh/nonuniform.xml")
+V = FunctionSpace(mesh, 'CG', 1, constrained_domain=PeriodicBoundary(Ld))
+W = VectorFunctionSpace(mesh, 'CG', 1, constrained_domain=PeriodicBoundary(Ld))
 
+#==============================================================================
+# INITIALIZING POPULATION
+#------------------------------------------------------------------------------
+
+pop = Population(mesh)
+distr = Distributor(V, Ld)
+poisson = PoissonSolver(V)
+
+initLangmuir(pop, Ld, 0, [0,0], 0.5, 2, 8)
+
+dt = 0.251327
+N = 30
+
+KE = np.zeros(N-1)
+PE = np.zeros(N-1)
+KE0 = kineticEnergy(pop)
+
+for n in range(1,N):
+	print("Computing timestep %d/%d"%(n,N-1))
+	rho = distr.distr(pop)
+	phi = poisson.solve(rho)
+	E = EField(phi)
+	PE[n-1] = potentialEnergy(pop, phi)
+	KE[n-1] = accel(pop,E,(1-0.5*(n==1))*dt)
+	movePeriodic(pop,Ld,dt)
+
+KE[0] = KE0
+
+plt.plot(KE,label="Kinetic Energy")
+plt.plot(PE,label="Potential Energy")
+plt.plot(KE+PE,label="Total Energy")
+plt.legend(loc='lower right')
+plt.grid()
+plt.xlabel("Timestep")
+plt.ylabel("Normalized Energy")
+plt.show()
+
+plot(rho)
+plot(phi)
+
+ux = Constant((1,0))
+Ex = project(inner(E,ux),V)
+plot(Ex)
+interactive()
--- punc.py
+++ punc.py
@@ -9,12 +9,12 @@
 # Imports important python 3 behaviour to ensure correct operation and
 # performance in python 2
 from __future__ import print_function, division
-
 import sys
 if sys.version_info.major == 2:
 	from itertools import izip as zip
 	range = xrange
 
+#import dolfin as df
 from dolfin import *
 import numpy as np
 from mpi4py import MPI as pyMPI
@@ -32,12 +32,15 @@ __UINT32_MAX__ = np.iinfo('uint32').max
 Ld = ...
 mesh = ...
 
-E = VectorFunctionSpace(...)
-rho = Function(...)
-phi = Function(...)
+V = FunctionSpace(mesh, 'CG', 1, constr=constr)
+Vv = VectorFunctionSpace(mesh, 'CG', 1, constr=constr)
+
+E = Function(Vv)
+rho = Function(V)
+phi = Function(V)
 
 
-fsolver = PeriodicPoissonSolver()
+fsolver = PeriodicPoissonSolver(V)
 pop = Population()
 acc = Accelerator()
 mov = Mover()
@@ -47,14 +50,14 @@ dist = Distributer()
 
 # Adding electrons
 vd, vth = ..., ...
-q, m, N = stdParticleParameters(mesh,Npc,qm,mme)
+q, m, N = stdSpecie(mesh, Ld, -1, 1, 8)
 xs = randomPoints(lambda x: ..., Ld, N)
 vs = maxwellian(vd, vth)
 pop.addParticles(xs,vs,q,m)
 
 # Adding ions
 vd, vth = ..., ...
-q, m, N = stdParticleParameters(mesh,Npc,qm,mme)
+q, m, N = stdSpecie(mesh, Ld, -1, 1, 8)
 xs = randomPoints(lambda x: ..., Ld, N)
 vs = maxwellian(vd, vth)
 pop.addParticles(xs,vs,q,m)
@@ -80,93 +83,94 @@ KE[0] = KE0
 
 """
 
-class PeriodicPoissonSolver(object):
+def stdSpecie(mesh, Ld, q, m, N, q0=-1.0, m0=1.0, wp0=1.0, count='per cell'):
+	"""
+	Returns standard normalized particle parameters to use for a specie. The
+	inputs q and m are the charge and mass of the specie in elementary charges
+	and electron masses, respectively. N is the number of particles per cell
+	unless count='total' in which case it is the total number of simulation
+	particles in the domain. For instance to create 8 ions per cell (having
+	mass of 1836 electrons and 1 positive elementary charge):
 
-	def solve(self,rho,E):
+		q, m, N = stdSpecie(mesh, Ld, 1, 1836, 8)
 
-		L = self.rho*self.phi_*dx
-		b = assemble(L)
+	The returned values are the normalized charge q, mass m and total number of
+	simulation particles N. The normalization is such that the angular electron
+	plasma frequency will be 1.
 
-		self.null_space.orthogonalize(b);
+	Alternatively, the reference charge q0 and mass m0 which should yield a
+	angular plasma frequency of 1, or for that sake any other frequency wp0,
+	could be set through the kwargs. For example, in this case the ions will
+	have a plasma frequency of 0.1:
 
-		self.solver.solve(self.phi.vector(), b)
+		q, m, N = stdSpecie(mesh, Ld, 1, 1836, 8, q0=1, m0=1836, wp0=0.1)
+	"""
 
-		self.E = project(-grad(self.phi), self.CG1V)
+	assert count in ['per cell','total']
 
-class PoissonSolver2(object):
+	if count=='total':
+		Np = N
+	else:
+		Np = N*mesh.num_cells()
 
-	def __init__(self, mesh, Ld, bnd):
+	mul = (np.prod(Ld)/np.prod(Np))*(wp0**2)*m0/(q0**2)
+	return q*mul, m*mul, Np
 
-		assert(bnd in ['periodic','dirichlet'])
+class PoissonSolver:
 
-		#
-		# FUNCTION SPACES AND BOUNDARY CONDITIONS
-		#
-		self.bnd = bnd
-		if(bnd=='periodic'): constr = PeriodicBoundary(Ld)
-		else: constr = None
+	def __init__(self, V):
 
-		self.mesh = mesh
-		self.DG0  =       FunctionSpace(mesh,'DG',0,constrained_domain=constr)
-		self.CG1  =       FunctionSpace(mesh,'CG',1,constrained_domain=constr)
-		self.CG1V = VectorFunctionSpace(mesh,'CG',1,constrained_domain=constr)
+		self.solver = PETScKrylovSolver('gmres', 'hypre_amg')
+		self.solver.parameters['absolute_tolerance'] = 1e-14
+		self.solver.parameters['relative_tolerance'] = 1e-12
+		self.solver.parameters['maximum_iterations'] = 1000
 
-		if(bnd=='dirichlet'):
-			bc = DirichletBC(self.CG1,Constant(0),extBnd)
+		self.V = V
 
-		#
-		# INITIALIZE SOLVER
-		#
-		phi = TrialFunction(self.CG1)
-		phi_ = TestFunction(self.CG1)
+		phi = TrialFunction(V)
+		phi_ = TestFunction(V)
 
 		a = inner(nabla_grad(phi), nabla_grad(phi_))*dx
 		A = assemble(a)
 
-		self.solver = PETScKrylovSolver("cg")
 		self.solver.set_operator(A)
-
 		self.phi_ = phi_
-		self.phi = Function(self.CG1)
-		self.rho = Function(self.CG1)
-		self.E = Function(self.CG1V)
 
-		if(bnd=='periodic'):
-			# Remove null-vector from solution space
-			null_vec = Vector(self.phi.vector())
-			self.CG1.dofmap().set(null_vec, 1.0)
-			null_vec *= 1.0/null_vec.norm("l2")
-			self.null_space = VectorSpaceBasis([null_vec])
-			as_backend_type(A).set_nullspace(self.null_space)
+		phi = Function(V)
+		null_vec = Vector(phi.vector())
+		V.dofmap().set(null_vec, 1.0)
+		null_vec *= 1.0/null_vec.norm("l2")
 
-	def solve(self):
+		self.null_space = VectorSpaceBasis([null_vec])
+		as_backend_type(A).set_nullspace(self.null_space)
 
-		L = self.rho*self.phi_*dx
+	def solve(self, rho):
+
+		L = rho*self.phi_*dx
 		b = assemble(L)
+		self.null_space.orthogonalize(b);
 
-		if(self.bnd=='periodic'):
-			self.null_space.orthogonalize(b);
+		phi = Function(self.V)
+		self.solver.solve(phi.vector(), b)
 
-		self.solver.solve(self.phi.vector(), b)
-
-		self.E = project(-grad(self.phi), self.CG1V)
+		return phi
 
 class Accel:
 
 	def __init__(self, mesh, bnd):
 		pass
 
-class Distr:
+class Distributor:
 
-	def __init__(self, mesh, Ld, bnd="periodic"):
+	def __init__(self, V, Ld, bnd="periodic"):
 
 		assert bnd=="periodic"
 
-		self.mesh = mesh
-		self.CG1 = FunctionSpace(mesh, 'CG', 1)
+		self.V = V
+		self.mesh = V.mesh()
 
-		vertices = mesh.coordinates()
-		dofs = vertex_to_dof_map(self.CG1)
+		vertices = self.mesh.coordinates()
+		dofs = vertex_to_dof_map(self.V)
 
 		# Remove those on upper bound (admittedly inefficient)
 		i = 0
@@ -204,8 +208,8 @@ class Distr:
 		#dvArr = [vcell['volume'] for vcell in voronoi]
 		dvInvArr = [vcell['volume']**(-1) for vcell in voronoi]
 
-		#self.dv = Function(self.CG1)
-		self.dvInv = Function(self.CG1)
+		#self.dv = Function(self.V)
+		self.dvInv = Function(self.V)
 
 		# There must be some way to avoid this loop
 		for i in range(len(dvInvArr)):
@@ -218,18 +222,18 @@ class Distr:
 		# cheaper to multiply by its inverse we've computed self.dvInv too.
 		# We actually don't need self.dv except for debugging.
 
-	def distr(self,pop,rho):
+	def distr(self,pop):
 		# rho assumed to be CG1
 
-		element = self.CG1.dolfin_element()
+		element = self.V.dolfin_element()
 		sDim = element.space_dimension() # Number of nodes per element
 		basisMatrix = np.zeros((sDim,1))
 
-		rho.vector()[:] = 0
+		rho = Function(self.V)
 
 		for cell in cells(self.mesh):
 			cellindex = cell.index()
-			dofindex = self.CG1.dofmap().cell_dofs(cellindex)
+			dofindex = self.V.dofmap().cell_dofs(cellindex)
 
 			accum = np.zeros(sDim)
 			for particle in pop[cellindex]:
@@ -246,12 +250,12 @@ class Distr:
 		# Divide by volume of Voronoi cell
 		rho.vector()[:] *= self.dvInv.vector()
 
+		return rho
+
 class Population(list):
 
-	def __init__(self, CG1, Ld):
-		self.CG1 = CG1
-		self.mesh = CG1.mesh()
-		self.Ld = Ld
+	def __init__(self, mesh):
+		self.mesh = mesh
 
 		# Allocate a list of particles for each cell
 		for cell in cells(self.mesh):
@@ -280,72 +284,27 @@ class Population(list):
 		vZero = np.zeros(self.gDim)
 		self.particle0 = Particle(vZero,vZero,1,1)
 
-		self.compVoronoi(Ld)
+	def addParticles(self, xs, vs=None, qs=None, ms=None):
+		"""
+		Adds particles to the population and locates them on their home
+		processor. xs is a list/array of position vectors. vs, qs and ms may
+		be lists/arrays of velocity vectors, charges, and masses,
+		respectively, or they may be only a single velocity vector, mass
+		and/or charge if all particles should have the same value.
+		"""
 
-	def compVoronoi(self, Ld, bnd="periodic"):
-
-		assert bnd=="periodic"
-
-		mesh = self.CG1.mesh()
-		vertices = mesh.coordinates()
-		dofs = vertex_to_dof_map(self.CG1)
-
-		# Remove those on upper bound (admittedly inefficient)
-		i = 0
-		while i<len(vertices):
-			if any([near(a,b) for a,b in zip(vertices[i],list(Ld))]):
-				vertices = np.delete(vertices,[i],axis=0)
-				dofs = np.delete(dofs,[i],axis=0)
-			else:
-				i = i+1
-
-		# Sort vertices to appear in the order FEniCS wants them for the DOFs
-		sortIndices = np.argsort(dofs)
-		vertices = vertices[sortIndices]
-
-		nDims = len(Ld)
-		limits = np.zeros([nDims,2])
-		limits[:,1] = Ld
-
-		# ~5 particles (vertices) per block yields better performance.
-		nParticles = self.mesh.num_vertices()
-		nBlocks = nParticles/5.0
-		nBlocksPerDim = int(nBlocks**(1/nDims)) # integer feels safer
-		blockSize = np.prod(Ld)**(1/nDims)/nBlocksPerDim
-
-		if nParticles>24000:
-			print("Warning: The pyvoro library often experience problems with many particles. This despite the fact that voro++ should be well suited for big problems.")
-
-		if nDims==1:
-			error("1D voronoi not implemented yet")
-		if nDims==2:
-			voronoi = pyvoro.compute_2d_voronoi(vertices,limits,blockSize,periodic=[True]*2)
-		if nDims==3:
-			voronoi = pyvoro.compute_voronoi(vertices,limits,blockSize,periodic=[True]*3)
-
-		#dvArr = [vcell['volume'] for vcell in voronoi]
-		dvInvArr = [vcell['volume']**(-1) for vcell in voronoi]
-
-		#self.dv = Function(self.CG1)
-		self.dvInv = Function(self.CG1)
-
-		# There must be some way to avoid this loop
-		for i in range(len(dvInvArr)):
-			#self.dv.vector()[i] = dvArr[i]
-			self.dvInv.vector()[i] = dvInvArr[i]
-
-		# self.dv is now a FEniCS function which on the vertices of the FEM mesh
-		# equals the volume of the Voronoi cells created from those vertices.
-		# It's meaningless to evaluate self.dv in-between vertices. Since it's
-		# cheaper to multiply by its inverse we've computed self.dvInv too.
-		# We actually don't need self.dv except for debugging.
-
-	def addParticles(self, xs, vs, qs, ms):
-		# Adds particles to the population and locates them on their home
-		# processor. xs is a list/array of position vectors. vs, qs and ms may
-		# be lists/arrays of velocity vectors, charges, and masses,
-		# respectively, or they may be only a single velocity vector, mass
-		# and/or charge if all particles should have the same value.
+		if vs==None or qs==None or ms==None:
+			assert isinstance(xs,list)
+			if len(xs)==0:
+				return
+			assert isinstance(xs[0],Particle)
+			ps = xs
+			xs = [p.x for p in ps]
+			vs = [p.v for p in ps]
+			qs = [p.q for p in ps]
+			ms = [p.m for p in ps]
+			addParticles(xs, vs, qs, ms)
+			return
 
 		# Expand input to lists/arrays if necessary
 		if len(np.array(vs).shape)==1: vs = np.tile(vs,(len(xs),1))
@@ -370,15 +329,17 @@ class Population(list):
 			assert nMissing==0,'%d particles are not located in mesh'%nMissing
 
 	def relocate(self):
-		# Relocate particles on cells and processors
-		# Map such that map[old_cell] = [(new_cell, particle_id), ...]
-		# Ie new destination of particles formerly in old_cell
+		"""
+		Relocate particles on cells and processors
+		map such that map[old_cell] = [(new_cell, particle_id), ...]
+		i.e. new destination of particles formerly in old_cell
+		"""
 		new_cell_map = defaultdict(list)
 		for dfCell in cells(self.mesh):
 			cindex = dfCell.index()
 			cell = self[cindex]
 			for i, particle in enumerate(cell):
-				point = Point(*particle.pos)
+				point = Point(*particle.x)
 				# Search only if particle moved outside original cell
 				if not dfCell.contains(point):
 					found = False
@@ -455,34 +416,6 @@ class Population(list):
 			point = Point(*particle)
 		return self.tree.compute_first_entity_collision(point)
 
-	def distr(self,rho):
-		# rho assumed to be CG1
-
-		element = self.CG1.dolfin_element()
-		sDim = element.space_dimension() # Number of nodes per element
-		basisMatrix = np.zeros((sDim,1))
-
-		rho.vector()[:] = 0
-
-		for cell in cells(self.mesh):
-			cellindex = cell.index()
-			dofindex = self.CG1.dofmap().cell_dofs(cellindex)
-
-			accum = np.zeros(sDim)
-			for particle in self[cellindex]:
-
-				element.evaluate_basis_all(	basisMatrix,
-											particle.x,
-											cell.get_vertex_coordinates(),
-											cell.orientation())
-
-				accum += particle.q * basisMatrix.T[0]
-
-			rho.vector()[dofindex] += accum
-
-		# Divide by volume of Voronoi cell
-		rho.vector()[:] *= self.dvInv.vector()
-
 class Particle:
 	__slots__ = ['x', 'v', 'q', 'm']
 	def __init__(self, x, v, q, m):
@@ -529,6 +462,10 @@ def randomPoints(pdf, Ld, N, pdfMax=1):
 
 	Ld = np.array(Ld) # In case it's a list
 	dim = len(Ld)
+
+	assert not isinstance(pdf(np.ones(dim)),(list,np.ndarray)) ,\
+		"pdf returns non-scalar value"
+
 	points = np.array([]).reshape(0,dim)
 
 	while len(points)<N:
@@ -546,8 +483,160 @@ def randomPoints(pdf, Ld, N, pdfMax=1):
 	return points
 
 def maxwellian(vd, vth, N):
-	# Returns N maxwellian velocity vectors with drift and thermal velocities
-	# vd and vth, respectively. If you have a list/array of position vectors
-	# pos an equivalen way to create them would be
-	# 	np.random.normal(vd, vth, pos.shape)
-	return np.random.normal(vd, vth, (N,len(vd)) )
+	"""
+	Returns N maxwellian velocity vectors with drift and thermal velocity
+	vectors vd and vth, respectively, in an (N,d) array where d is the number
+	of geometric dimensions. If you have an (N,d) array xs (e.g. position
+	vectors) the following call would also be valid:
+
+		maxwellian(vd, vth, xs.shape)
+
+	or even
+
+		np.random.normal(vd, vth, xs.shape)
+
+	where np stands for numpy. However, np.random.normal fails when one of the
+	elements in the vector vth is zero. maxwellian replaces zeroes with the
+	machine epsilon.
+
+	Either vd or vth may be a scalar, or both if N is a shape-tuple.
+	"""
+
+	if isinstance(N,int):
+		d = max(len(vd),len(vth))
+	else:
+		(N,d) = N
+
+	# Replace zeroes in vth for machine epsilon
+	if isinstance(vth,(float,int)): vth = [vth]
+	vth = np.array(vth,dtype=float)
+	indices = np.where(vth==0.0)[0]
+	vth[indices] = np.finfo(float).eps
+
+	return np.random.normal(vd, vth, (N,d))
+
+def initLangmuir(pop, Ld, vd, vth, A, mode, Npc):
+	"""
+	Initializes population pop with particles corresponding to electron-ion
+	plasma oscillations with amplitude A in density, thermal velocity vth[0]
+	and vth[1] for electrons and ions, respectively, and drift velocity vector
+	vd. mesh and Ld is the mesh and it's size, while Npc is the number of
+	simulation particles per cell per specie. Normalization is such that
+	angular electron plasma frequency is one. mode is the number of half
+	periods per domain length in the x-direction.
+	"""
+
+	mesh = pop.mesh
+
+	# Adding electrons
+	q, m, N = stdSpecie(mesh, Ld, -1, 1, Npc)
+	pdf = lambda x: 1+A*np.sin(mode*2*np.pi*x[0]/Ld[0])
+	xs = randomPoints(pdf, Ld, N, pdfMax=1+A)
+	vs = maxwellian(vd, vth[0], xs.shape)
+	pop.addParticles(xs,vs,q,m)
+
+	# Adding ions
+	q, m, N = stdSpecie(mesh, Ld, 1, 1836, Npc)
+	pdf = lambda x: 1
+	xs = randomPoints(pdf, Ld, N)
+	vs = maxwellian(vd, vth[1], xs.shape)
+	pop.addParticles(xs,vs,q,m)
+
+def EField(phi):
+	V = phi.ufl_function_space()
+	mesh = V.mesh()
+	degree = V.ufl_element().degree()
+	constr = V.constrained_domain
+	W = VectorFunctionSpace(mesh, 'CG', degree, constrained_domain=constr)
+	return project(-grad(phi), W)
+
+def accel(pop, E, dt):
+
+	W = E.function_space()
+	mesh = W.mesh()
+	element = W.dolfin_element()
+	sDim = element.space_dimension()  # Number of nodes per element
+	vDim = element.value_dimension(0) # Number of values per node (=geom. dim.)
+	basisMatrix = np.zeros((sDim,vDim))
+	coefficients = np.zeros(sDim)
+
+	KE = 0.0
+	for cell in cells(mesh):
+
+		E.restrict(	coefficients,
+					element,
+					cell,
+					cell.get_vertex_coordinates(),
+					cell)
+
+		for particle in pop[cell.index()]:
+			element.evaluate_basis_all(	basisMatrix,
+										particle.x,
+										cell.get_vertex_coordinates(),
+										cell.orientation())
+
+			Ei = np.dot(coefficients, basisMatrix)[:]
+
+			m = particle.m
+			q = particle.q
+
+			inc = dt*(q/m)*Ei
+			vel = particle.v
+
+			KE += 0.5*m*np.dot(vel,vel+inc)
+
+			particle.v += inc
+
+	return KE
+
+def movePeriodic(pop, Ld, dt):
+	for cell in pop:
+		for particle in cell:
+			particle.x += dt*particle.v
+			particle.x %= Ld
+	pop.relocate()
+
+def kineticEnergy(pop):
+	"""
+	Computes kinetic energy at current velocity time step.
+	Useful for the first (zeroth) time step before velocity has between
+	advanced half a timestep. To get velocity between two velocity time
+	steps (e.g. at integer steps after the start-up) use accel() return.
+	"""
+	KE = 0
+	for cell in pop:
+		for particle in cell:
+			m = particle.m
+			v = particle.v
+			KE += 0.5*m*np.dot(v,v)
+	return KE
+
+def potentialEnergy(pop,phi):
+
+	PE = 0
+
+	V = phi.function_space()
+	element = V.dolfin_element()
+	sDim = element.space_dimension()  # Number of nodes per element
+	basisMatrix = np.zeros((sDim,1))
+	coefficients = np.zeros(sDim)
+
+	for cell in cells(pop.mesh):
+		phi.restrict(	coefficients,
+						element,
+						cell,
+						cell.get_vertex_coordinates(),
+						cell)
+
+		for particle in pop[cell.index()]:
+			element.evaluate_basis_all(	basisMatrix,
+										particle.x,
+										cell.get_vertex_coordinates(),
+										cell.orientation())
+
+			phii = np.dot(coefficients, basisMatrix)[:]
+
+			q = particle.q
+			PE += 0.5*q*phii
+
+	return PE
