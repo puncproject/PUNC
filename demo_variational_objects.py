9e6917f11fd97448d220b1c824063837f55b8555
sigvaldm <sigvaldm@fys.uio.no>
2017-11-01 10:10:57 +0100

    Further investigations of iterative solvers and the variational approach. Includes multiple objects

--- demo_variational_objects.py
+++ demo_variational_objects.py
@@ -8,34 +8,65 @@ from dolfin import *
 import numpy as np
 import matplotlib.pyplot as plt
 from punc import *
+import time
 
-mesh = Mesh("mesh/two_obj.xml")
-boundaries = MeshFunction("size_t", mesh, "mesh/two_obj_facet_region.xml")
+mesh = Mesh("mesh/two_obj_fine.xml")
+boundaries = MeshFunction("size_t", mesh, "mesh/two_obj_fine_facet_region.xml")
 ext_bnd_id = 17
 int1_bnd_id = 18
 int2_bnd_id = 19
 
-# Charge and potential difference between objects (if connected)
-Q1 = Constant(15.)
-Q2 = Constant(20.)
+# Simulation settings
+Q1 = Constant(15.) # Object 1 charge
+Q2 = Constant(20.) # Object 2 charge
 V12 = Constant(3.)
-connected = True
+connected = True # Connect voltage source of voltage V12 between objects
+grounded = False # Wether objects should be grounded (Dirichlet)
+direct = False # Use direct solver instead of iterative solver?
+method = 'tfqmr'
+preconditioner = 'none'
 
-cell = mesh.ufl_cell()
-V = FiniteElement("Lagrange", cell, 1)
-R = FiniteElement("Real", cell, 0)
+# Benchmark results on Sigvald's computer.
+#
+# Connected (not grounded) objects:
+# TFQMR/none    2.20s
+# BICGSTAB/ICC  2.25s
+# TFQMR/ICC     6.95s
+# TFQMR/Jacobi 13.60s
+# GMRES/Jacobi 13.80s
+# GMRES/None   30.10s
+# GMRES/ICC    38.50s
+#
+# Non-connected, floating objects didn't seem to alter the results
+# significantly for TFQMR/none.
+#
+# Grounded (Dirichlet) objects (as for Capacitance Matrix method).
+# GMRES/hypre_AMG   0.16s
+# BICGSTAB/ICC      0.41s
+# TFQMR/none        0.43s
 
-W = FunctionSpace(mesh, df.MixedElement([V, R, R]))
-u, c1, c2 = TrialFunctions(W)
-v, d1, d2 = TestFunctions(W)
 
-# W = FunctionSpace(mesh, df.MixedElement([V, R]))
-# u, c1 = TrialFunctions(W)
-# v, d1 = TestFunctions(W)
+if grounded:
+    W = FunctionSpace(mesh, "CG", 1)
+    u = TrialFunction(W)
+    v = TestFunction(W)
 
-ext_bc = DirichletBC(W.sub(0), df.Constant(0), boundaries, ext_bnd_id)
-int1_bc = FloatingBC(W.sub(0), boundaries, int1_bnd_id)
-int2_bc = FloatingBC(W.sub(0), boundaries, int2_bnd_id)
+    ext_bc = DirichletBC(W, Constant(0), boundaries, ext_bnd_id)
+    int1_bc = DirichletBC(W, Constant(0), boundaries, int1_bnd_id)
+    int2_bc = DirichletBC(W, Constant(0), boundaries, int2_bnd_id)
+
+else:
+    cell = mesh.ufl_cell()
+    V = FiniteElement("Lagrange", cell, 1)
+    R = FiniteElement("Real", cell, 0)
+
+    W = FunctionSpace(mesh, MixedElement([V, R, R]))
+    u, c1, c2 = TrialFunctions(W)
+    v, d1, d2 = TestFunctions(W)
+
+    ext_bc = DirichletBC(W.sub(0), Constant(0), boundaries, ext_bnd_id)
+    int1_bc = FloatingBC(W.sub(0), boundaries, int1_bnd_id)
+    int2_bc = FloatingBC(W.sub(0), boundaries, int2_bnd_id)
 
 # rho = Constant(0.)
 rho = df.Expression("100*x[1]", degree=3)
@@ -48,8 +79,13 @@ dsi = ds1+ds2
 S1 = assemble(Constant(1.)*ds1)
 S2 = assemble(Constant(1.)*ds2)
 
+if grounded:
 
-if connected:
+    # TWO OBJECTS GROUNDED
+    a = inner(grad(u), grad(v)) * dx
+    L = inner(rho, v) * dx
+
+elif connected:
 
     # TWO OBJECTS BIASED WRT ONE ANOTHER
     a = inner(grad(u), grad(v)) * dx -\
@@ -88,9 +124,25 @@ int1_bc.apply(b)
 int2_bc.apply(A)
 int2_bc.apply(b)
 
-df.solve(A, wh.vector(), b)
+if direct:
+    solve(A, wh.vector(), b)
+else:
+    solver = KrylovSolver(method,preconditioner)
+    solver.parameters['absolute_tolerance'] = 1e-14
+    solver.parameters['relative_tolerance'] = 1e-12 #e-12
+    solver.parameters['maximum_iterations'] = 100000
+    # solver.parameters['monitor_convergence'] = True
 
-uh, ph, qh = wh.split(deepcopy=True)
+    print("Started solving")
+    t0 = time.time()
+    solver.solve(A, wh.vector(), b)
+    t1 = time.time()
+    print("Time:",t1-t0)
+
+if grounded:
+    uh = wh
+else:
+    uh, ph, qh = wh.split(deepcopy=True)
 
 Q1m = assemble(dot(grad(uh), n) * ds1)
 Q2m = assemble(dot(grad(uh), n) * ds2)
---
+++ demo_variational_sphere.py
@@ -0,0 +1,117 @@
+from __future__ import print_function, division
+import sys
+if sys.version_info.major == 2:
+    from itertools import izip as zip
+    range = xrange
+
+from dolfin import *
+import numpy as np
+import matplotlib.pyplot as plt
+from punc import *
+import time
+
+mesh = Mesh("mesh/sphereinsphere_fine.xml")
+boundaries = MeshFunction("size_t", mesh, "mesh/sphereinsphere_fine_facet_region.xml")
+ext_bnd_id = 58
+int_bnd_id = 59
+
+# Simulation settings
+Q = Constant(10.) # Object 1 charge
+grounded = True # Wether objects should be grounded (Dirichlet)
+direct = False # Use direct solver instead of iterative solver?
+method = 'gmres'
+preconditioner = 'ilu'
+
+if grounded:
+    W = FunctionSpace(mesh, "CG", 1)
+    u = TrialFunction(W)
+    v = TestFunction(W)
+
+    ext_bc = DirichletBC(W, Constant(0), boundaries, ext_bnd_id)
+    int_bc = DirichletBC(W, Constant(0), boundaries, int_bnd_id)
+
+else:
+    cell = mesh.ufl_cell()
+    V = FiniteElement("Lagrange", cell, 1)
+    R = FiniteElement("Real", cell, 0)
+
+    W = FunctionSpace(mesh, MixedElement([V, R]))
+    u, c = TrialFunctions(W)
+    v, d = TestFunctions(W)
+
+    ext_bc = DirichletBC(W.sub(0), Constant(0), boundaries, ext_bnd_id)
+    int_bc = FloatingBC(W.sub(0), boundaries, int_bnd_id)
+
+# rho = Constant(0.)
+rho = df.Expression("100*x[1]", degree=3)
+n = FacetNormal(mesh)
+dss = Measure("ds", domain=mesh, subdomain_data=boundaries)
+dsi = dss(int_bnd_id)
+
+S = assemble(Constant(1.)*dsi)
+
+if grounded:
+
+    a = inner(grad(u), grad(v)) * dx
+    L = inner(rho, v) * dx
+
+else:
+
+    # TWO OBJECTS BIASED WRT ONE ANOTHER
+    a = inner(grad(u), grad(v)) * dx -\
+        inner(v, dot(grad(u), n)) * dsi +\
+        inner(c, dot(grad(v), n)) * dsi +\
+        inner(d, dot(grad(u), n)) * dsi
+
+    L = inner(rho, v) * dx +\
+        inner(Q/S, d) * dsi
+
+wh = df.Function(W)
+
+print("Assembling matrix")
+A = df.assemble(a)
+b = df.assemble(L)
+print("Applying boundary conditions")
+ext_bc.apply(A)
+ext_bc.apply(b)
+int_bc.apply(A)
+int_bc.apply(b)
+
+if direct:
+    solve(A, wh.vector(), b)
+else:
+    solver = PETScKrylovSolver(method,preconditioner)
+    solver.parameters['absolute_tolerance'] = 1e-14
+    solver.parameters['relative_tolerance'] = 1e-10 #e-12
+    solver.parameters['maximum_iterations'] = 100000
+    solver.parameters['monitor_convergence'] = True
+    solver.parameters['nonzero_initial_guess'] = True
+
+    print("Setting operator (computing preconditioning?)")
+    solver.set_operator(A)
+    solver.set_reuse_preconditioner(True)
+
+    print("Started solving 1st time")
+    t0 = time.time()
+    solver.solve(wh.vector(), b)
+    # solver.solve(A, wh.vector(), b)
+    t1 = time.time()
+    print("Time:",t1-t0)
+
+    print("Started solving 2nd time")
+    t0 = time.time()
+    # solver.solve(A, wh.vector(), b)
+    solver.solve(wh.vector(), b)
+    t1 = time.time()
+    print("Time:",t1-t0)
+
+if grounded:
+    uh = wh
+else:
+    uh, ph = wh.split(deepcopy=True)
+
+Qm = assemble(dot(grad(uh), n) * dsi)
+print("Object charge: ", Qm)
+
+# df.plot(uh, interactive=True)
+# df.File("phi.pvd") << uh
