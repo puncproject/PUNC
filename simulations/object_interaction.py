d26a71b57086a4cd8e82d8b4afdfed0709b6a6b5
sigvaldm <sigvaldm@fys.uio.no>
2018-07-04 15:54:37 +0200

    Licensed object_interaction.py

--- simulations/object_interaction.py
+++ simulations/object_interaction.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
+
+# Copyright (C) 2017, Sigvald Marholm and Diako Darian
+#
+# This file is part of PUNC.
+#
+# PUNC is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# PUNC is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# PUNC. If not, see <http://www.gnu.org/licenses/>.
+
 """
 PUNC program for doing plasma-object interaction simulations, e.g. reproducing
 the results of Laframboise. Usage:
