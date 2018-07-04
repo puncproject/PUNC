632148fc39101a485225f9560fe514dd84960a8b
sigvaldm <sigvaldm@fys.uio.no>
2018-07-04 15:53:22 +0200

    Updated licenses

--- punc/ConstantBC.py
+++ punc/ConstantBC.py
@@ -1,18 +1,19 @@
-# Copyright (C) 2017, Sigvald Marholm and Diako Darian
+# Copyright (C) 2017, Sigvald Marholm, Diako Darian and Mikael Mortensen
 #
-# This file is part of ConstantBC.
+# This file is part of PUNC.
 #
-# ConstantBC is free software: you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
+# PUNC is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
 # Foundation, either version 3 of the License, or (at your option) any later
 # version.
 #
-# ConstantBC is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+# PUNC is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
 #
 # You should have received a copy of the GNU General Public License along with
-# ConstantBC.  If not, see <http://www.gnu.org/licenses/>.
+# PUNC. If not, see <http://www.gnu.org/licenses/>.
 
 import dolfin as df
 import numpy as np
